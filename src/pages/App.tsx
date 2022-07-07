423c771d4f5fde8f61851a826df2868b5a533040
AIstilli <andre.istilli@gmail.com>
2022-07-07 17:28:43 -0300

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Cronometro from "../components/Cronometro";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import { ITarefa } from "../types/tarefa";
+import style from "./App.module.scss";
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
+      <Cronometro />
+    </div>
+  );
+}
+
+export default App;
+/* Depois de instalar o "typescript-plugin-css-modules", adicionar o plugin no tsconfig, modifica-se o nome dos arquivos .scss e a importação dentro do .tsx. 
+Depois modifica-se o className na tag.
+*/
