cee47f8f2f2d5d79a862ead9cfc4ede18a3ec9c6
AIstilli <andre.istilli@gmail.com>
2022-07-08 18:09:01 -0300

    Fim do curso

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 import { v4 as uuidv4 } from "uuid";
 
-class Formulario extends React.Component<{
+interface Props {
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}
+
+function Formulario({ setTarefas }: Props) {
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState("00:00");
+
+  function adicionaTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      {
+        tarefa,
+        tempo,
+        selecionado: false,
+        completado: false,
+        id: uuidv4(),
+      },
+    ]);
+    setTarefa("");
+    setTempo("00:00");
+  }
+
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionaTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          id="tarefa"
+          value={tarefa}
+          onChange={(evento) => setTarefa(evento.target.value)}
+          placeholder="O que você quer estudar"
+          required
+        ></input>
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          id="tempo"
+          value={tempo}
+          onChange={(evento) => setTempo(evento.target.value)}
+          min="00:00:00"
+          max="01:30:00"
+          required
+        ></input>
+      </div>
+      <Botao type="submit">Adicionar</Botao>
+    </form>
+  );
+}
+
+/*
+class Formulario1 extends React.Component<{
   setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
 }> {
   state = {
@@ -70,4 +128,6 @@ class Formulario extends React.Component<{
     );
   }
 }
+*/
+
 export default Formulario;
